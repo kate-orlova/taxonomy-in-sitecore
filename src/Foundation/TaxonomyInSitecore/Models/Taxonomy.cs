5aecd3f870d16419094afc163c6434be96a850e9
Kate Orlova <kate.orlova@deleteagency.com>
2020-06-03 10:23:22 +0500

    Defined a ContentTags property for Taxonomy class

--- src/Foundation/TaxonomyInSitecore/Models/Taxonomy.cs
+++ src/Foundation/TaxonomyInSitecore/Models/Taxonomy.cs
@@ -1,6 +1,9 @@
-﻿namespace Foundation.TaxonomyInSitecore.Models
+﻿using System.Collections.Generic;
+
+namespace Foundation.TaxonomyInSitecore.Models
 {
     public class Taxonomy
     {
+        public virtual IEnumerable<DictionaryEntry> ContentTags  {get; set;}
     }
 }
