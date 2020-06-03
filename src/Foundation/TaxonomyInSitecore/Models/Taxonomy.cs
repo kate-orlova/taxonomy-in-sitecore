87004ee347f1d8fa1be8e341bfd3bf5e327963ef
Kate Orlova <kate.orlova@deleteagency.com>
2020-06-03 10:28:53 +0500

    Defined ContentTagIds property in Taxonomy class

--- src/Foundation/TaxonomyInSitecore/Models/Taxonomy.cs
+++ src/Foundation/TaxonomyInSitecore/Models/Taxonomy.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Foundation.TaxonomyInSitecore.Models
 {
     public class Taxonomy
     {
         public virtual IEnumerable<DictionaryEntry> ContentTags  {get; set;}
+        public virtual List<Guid> ContentTagIds { get; set; }
     }
 }
