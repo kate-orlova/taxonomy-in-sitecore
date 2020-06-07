19cadb16ae75cfb0e81882a390e222cdfccefd00
Anna Gevel <anna.gevel@deleteagency.com>
2020-06-08 00:04:44 +0100

    added pipeline processor

---
+++ src/Foundation/TaxonomyInSitecore/Pipelines/ProcessTaxonomyProfiles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Foundation.TaxonomyInSitecore.Pipelines
+{
+    public class ProcessTaxonomyProfiles
+    {
+    }
+}
