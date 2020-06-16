0d6074f50f62fd3b2ba0d1c0ede3a604f846aea9
Anna Gevel <anna.gevel@deleteagency.com>
2020-06-16 23:25:13 +0100

    derived processor class from Sitecore base processor

--- src/Foundation/TaxonomyInSitecore/Pipelines/ProcessTaxonomyProfiles.cs
+++ src/Foundation/TaxonomyInSitecore/Pipelines/ProcessTaxonomyProfiles.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Sitecore.Analytics.Pipelines.ProcessItem;
 
 namespace Foundation.TaxonomyInSitecore.Pipelines
 {
-    public class ProcessTaxonomyProfiles
+    public class ProcessTaxonomyProfiles : ProcessItemProcessor
     {
+        public override void Process(ProcessItemArgs args)
+        {
+        }
     }
 }
