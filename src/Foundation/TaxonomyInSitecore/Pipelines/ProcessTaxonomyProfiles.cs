c65dd2e6692b4ea47c3cabb2065d17e893171204
Kate Orlova <kate.orlova@deleteagency.com>
2020-07-01 15:11:28 +0500

    Get tags of the context item in ProcessTaxonomyProfiles pipeline

--- src/Foundation/TaxonomyInSitecore/Pipelines/ProcessTaxonomyProfiles.cs
+++ src/Foundation/TaxonomyInSitecore/Pipelines/ProcessTaxonomyProfiles.cs
@@ -1,4 +1,7 @@
-﻿using Sitecore.Analytics.Pipelines.ProcessItem;
+﻿using Sitecore;
+using Sitecore.Diagnostics;
+using Sitecore.Analytics.Pipelines.ProcessItem;
+using Sitecore.Data.Fields;
 
 namespace Foundation.TaxonomyInSitecore.Pipelines
 {
@@ -6,6 +9,15 @@ namespace Foundation.TaxonomyInSitecore.Pipelines
     {
         public override void Process(ProcessItemArgs args)
         {
+            Assert.ArgumentNotNull(args, "args");
+            var item = Context.Item;
+
+            var taxonomyField = (MultilistField)item.Fields["Programming Language"];
+
+            foreach (var tag in taxonomyField.GetItems())
+            {
+                //TODO: assign tag Profile Cards to the context item
+            }
         }
     }
 }
