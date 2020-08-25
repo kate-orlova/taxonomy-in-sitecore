edaaa4029624e59a33ae0292cee9f9336ba7cec7
Kate Orlova <kate.orlova@deleteagency.com>
2020-08-25 23:22:10 +0500

    Implemented a Split() method for string values

--- src/Foundation/TaxonomyInSitecore/Extensions/StringExtensions.cs
+++ src/Foundation/TaxonomyInSitecore/Extensions/StringExtensions.cs
@@ -1,6 +1,23 @@
-﻿namespace Foundation.TaxonomyInSitecore.Extensions
+﻿using System;
+using System.Linq;
+
+namespace Foundation.TaxonomyInSitecore.Extensions
 {
     public static class StringExtensions
     {
+        public static bool NotEmpty(this string value)
+        {
+            return !string.IsNullOrEmpty(value) && !string.IsNullOrWhiteSpace(value);
+        }
+
+        public static string[] SafeSplit(this string value, string separator)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return Enumerable.Empty<string>().ToArray();
+            }
+
+            return value.Split(new[] {separator}, StringSplitOptions.RemoveEmptyEntries);
+        }
     }
 }
