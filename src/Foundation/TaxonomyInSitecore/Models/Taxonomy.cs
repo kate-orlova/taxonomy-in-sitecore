c3e1f3b70c7391f74bf21ad9fc6e141dc7eca781
Kate Orlova <kate.orlova@deleteagency.com>
2021-02-14 20:17:22 +0000

    Specified Tags property

--- src/Foundation/TaxonomyInSitecore/Models/Taxonomy.cs
+++ src/Foundation/TaxonomyInSitecore/Models/Taxonomy.cs
@@ -1,4 +1,6 @@
-﻿namespace Foundation.TaxonomyInSitecore.Models
+﻿using Foundation.TaxonomyInSitecore.Configuration;
+
+namespace Foundation.TaxonomyInSitecore.Models
 {
     public class Taxonomy
     {
@@ -16,6 +18,24 @@
                 {
                     _contextItem = Sitecore.Context.Item;
                     return _contextItem;
+                }
+            }
+        }
+        public virtual Sitecore.Data.Items.Item[] Tags
+        {
+            get
+            {
+                if (_tags != null)
+                {
+                    return _tags;
+                }
+                else
+                {
+
+                    Sitecore.Data.Fields.MultilistField multiselectField = this.ContextItem.Fields[ConfigSettings.TaxonomyField];
+
+                    _tags = multiselectField.GetItems();
+                    return _tags;
                 }
             }
         }
