eb1c7791d0b6382cf45242a340e26127beb542b9
Kate Orlova <kate.orlova@deleteagency.com>
2021-02-14 20:16:05 +0000

    Specified ContextItem property

--- src/Foundation/TaxonomyInSitecore/Models/Taxonomy.cs
+++ src/Foundation/TaxonomyInSitecore/Models/Taxonomy.cs
@@ -4,5 +4,20 @@
     {
         private Sitecore.Data.Items.Item _contextItem;
         private Sitecore.Data.Items.Item[] _tags;
+        public virtual Sitecore.Data.Items.Item ContextItem
+        {
+            get
+            {
+                if (_contextItem != null)
+                {
+                    return _contextItem;
+                }
+                else
+                {
+                    _contextItem = Sitecore.Context.Item;
+                    return _contextItem;
+                }
+            }
+        }
     }
 }
