f5c1b047d87db6e2db5372083c91d2dbc1a3551a
Anna Gevel <anna.gevel@deleteagency.com>
2021-02-21 18:57:14 +0000

    added a model for taxonomy page view event

---
+++ src/Feature/TaxonomyReporting/Models/TaxonomyPageViewEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Sitecore.XConnect;
+
+namespace TaxonomyReporting.Models
+{
+    public class TaxonomyPageViewEvent : Event
+    {
+        public TaxonomyPageViewEvent(Guid definitionId, DateTime timestamp) : base(definitionId, timestamp)
+        {
+        }
+    }
+}
