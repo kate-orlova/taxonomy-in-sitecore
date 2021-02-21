da95f726ebc9ca68a6e490fc072e1c3df506fde9
Anna Gevel <anna.gevel@deleteagency.com>
2021-02-21 19:16:17 +0000

    configured page view event model

--- src/Feature/TaxonomyReporting/Models/TaxonomyPageViewEvent.cs
+++ src/Feature/TaxonomyReporting/Models/TaxonomyPageViewEvent.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using Sitecore.XConnect;
 
 namespace TaxonomyReporting.Models
 {
     public class TaxonomyPageViewEvent : Event
     {
-        public TaxonomyPageViewEvent(Guid definitionId, DateTime timestamp) : base(definitionId, timestamp)
+        public TaxonomyPageViewEvent(List<string> taxonomyTags, DateTime timestamp) : base(TaxonomyPageViewEvent.EventDefinitionId, timestamp)
         {
+            TaxonomyTags = taxonomyTags;
         }
+
+        public static readonly Guid EventDefinitionId = new Guid("FCFA9424-0558-4892-AB30-BB70B9F51866");
+
+        public List<string> TaxonomyTags { get; set; }
     }
 }
