47ec8b557cc83359e5d6f722393320bca6a5a506
Anna Gevel <anngevel@gmail.com>
2021-02-23 23:55:10 +0000

    added processor for registering a custom event

---
+++ src/Feature/TaxonomyReporting/Pipelines/RegisterTaxonomyPageEvent.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.Analytics.Pipelines.ProcessItem;
+using Sitecore.Diagnostics;
+
+namespace TaxonomyReporting.Pipelines
+{
+    public class RegisterTaxonomyPageEvent : ProcessItemProcessor
+    {
+        public override void Process(ProcessItemArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, nameof(args));
+        }
+    }
+}
