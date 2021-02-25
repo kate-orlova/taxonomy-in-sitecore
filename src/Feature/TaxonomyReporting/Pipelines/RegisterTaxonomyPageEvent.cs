322bb93d1a767baa7e62daa76addf802619f3002
Anna Gevel <anngevel@gmail.com>
2021-02-25 23:43:54 +0000

    added method for getting tag names

--- src/Feature/TaxonomyReporting/Pipelines/RegisterTaxonomyPageEvent.cs
+++ src/Feature/TaxonomyReporting/Pipelines/RegisterTaxonomyPageEvent.cs
@@ -1,5 +1,9 @@
-﻿using Sitecore.Analytics.Pipelines.ProcessItem;
+﻿using Foundation.TaxonomyInSitecore.Configuration;
+using Sitecore.Analytics.Pipelines.ProcessItem;
+using Sitecore.Data.Items;
+using Sitecore.Data.Fields;
 using Sitecore.Diagnostics;
+using System.Linq;
 
 namespace TaxonomyReporting.Pipelines
 {
@@ -8,6 +12,24 @@ namespace TaxonomyReporting.Pipelines
         public override void Process(ProcessItemArgs args)
         {
             Assert.ArgumentNotNull((object)args, nameof(args));
+
+            var tagNames = GetTagNames(Sitecore.Context.Item);            
+        }
+
+        protected string GetTagNames(Item item)
+        {
+            if (item != null)
+            {
+                var taxonomyField = (MultilistField)item.Fields[ConfigSettings.TaxonomyField];
+
+                if (taxonomyField != null)
+                {
+                    var taxonomyItems = taxonomyField.GetItems();
+                    var tags = string.Join("|", taxonomyItems.Select(x => x.Name));
+                    return tags;
+                }
+            }
+            return string.Empty;
         }
     }
 }
