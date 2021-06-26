1fb65c65e40e2db75fd0ce51f5fe2ad97c2a5e8f
Kate Orlova <kate.orlova@deleteagency.com>
2021-06-26 19:39:03 +0100

    Implemented TaxonomyMetrics model

---
+++ src/Feature/TaxonomyReporting/Models/TaxonomyMetrics.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Analytics.Aggregation.Data.Model;
+using Sitecore.ExperienceAnalytics.Aggregation.FlexibleMetrics.Framework.Metrics;
+using System;
+
+namespace TaxonomyReporting.Models
+{
+    class TaxonomyMetrics : DictionaryValue, IMergeableMetric<TaxonomyMetrics>
+    {
+        public int Visits { get; set; }
+        public int EngagementValue { get; set; }
+
+        public T MergeWith<T>(T other) where T : TaxonomyMetrics, new()
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+
+            return new T
+            {
+                Visits = Visits + other.Visits,
+                EngagementValue = other.EngagementValue
+            };
+        }
+    }
+}
